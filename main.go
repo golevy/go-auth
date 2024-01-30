18b3ef2af968c1fbcade25784c4bcc4446eaed0f
Levy <golvwei@gmail.com>
2024-01-31 00:37:04 +0800

    feat: Cookie (#6)

--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 	"go-auth/database"
 	"go-auth/routes"
 )
 
 func main() {
+	err := godotenv.Load() // This will load the .env file
+	if err != nil {
+		panic("Could not load .env file")
+	}
+
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.Config{
+		AllowCredentials: true,
+	})
 
 	routes.Setup(app)
 
