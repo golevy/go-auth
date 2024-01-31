fafee2c81c26a023fe1d0247bcea210dd50a8a31
Levy <golvwei@gmail.com>
2024-01-31 22:06:12 +0800

    feat: User (#7)

--- models/user.go
+++ models/user.go
@@ -3,6 +3,6 @@ package models
 type User struct {
 	Id       uint   `json:"id"` // Standard field for the primary key
 	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"` // specifies column as unique
-	Password []byte `json:"password"`
+	Email    string `json:"email" gorm:"unique"` // Specifies column as unique
+	Password []byte `json:"-"`                   // Ignored in JSON serialization/deserialization.
 }
